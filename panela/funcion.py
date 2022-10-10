e5583e423e94a8220d358d35cb39a0a7f2651b1c
Augusto Kielbowicz <augusto.kiel@gmail.com>
2022-10-10 19:59:10 -0300

    refactorizar funcion

--- panela/funcion.py
+++ panela/funcion.py
@@ -2,34 +2,31 @@ from abc import ABC, abstractmethod
 
 class Funcion(ABC):
 
-    def __init__(self) -> None:
-        self.interna_ = lambda x: None
+    @abstractmethod
+    def op(self, valor):
+        pass
 
     def Evaluar(self, valor):
-        return self.interna_(valor)
+        return self.op(valor)
 
 class Constante(Funcion):
 
     def __init__(self, valor) -> None:
-        self.interna_ = lambda x: valor
+        self._valor = valor
+
+    def op(self, valor):  return self._valor
 
 class Identidad(Funcion):
 
-    def __init__(self) -> None:
-        self.interna_ = lambda x: x
+    def op(self, valor): return valor
 
 class Operacion(Funcion):
 
     def __init__(self, argumento):
         self.interna_ = argumento
 
-    @abstractmethod
-    def op(self, valor):
-        pass
-
     def Evaluar(self, valor):
         return self.op(self.interna_.Evaluar(valor))
-
 
 class InversaAditiva(Operacion):
 
