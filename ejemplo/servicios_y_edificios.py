55a374b858a02d4e4fdd2938cd2101347a0e1595
Augusto Kielbowicz <augusto.kiel@gmail.com>
2022-11-03 19:33:50 -0300

    Agregar lista de precios

--- ejemplo/servicios_y_edificios.py
+++ ejemplo/servicios_y_edificios.py
@@ -6,19 +6,23 @@ x = """Edificacion,Id,Tipo
        casa,         4, oficina
     """
 
+precios = {"oficina": 0.8, "comercial": 5.0, "domestico": 1.0}
+
 def main(documento):
     lineas = documento.splitlines()
+    total = 0
     for linea in lineas[1:]:
         if len(linea.strip()) > 0:
-            fila = linea.split(',')
+            fila = linea.split(",")
             ed,id,tipo = [e.strip() for e in fila ]
             print(f"ID:{id},{ed},{tipo}")
-            if ed == 'casa':
-                print("casa")
-            elif ed == 'departamento':
-                print("depto")
+            if ed == "casa":
+                total += 1.0 * precios[tipo]
+            elif ed == "departamento":
+                total += 0.5 * precios[tipo]
             else:
                 print("desconocido")
+    print(f"Consumo total: {total}")
 
 
 if __name__ == "__main__":
