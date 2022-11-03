6bdd46560b565a21358bf47f80419675dcea60fc
Augusto Kielbowicz <augusto.kiel@gmail.com>
2022-11-03 20:06:21 -0300

    Agregar escala de precios

--- ejemplo/servicios_y_edificios.py
+++ ejemplo/servicios_y_edificios.py
@@ -1,9 +1,9 @@
-x = """Edificacion,Id,Tipo
-       casa,         0, domestico
-       departamento, 1, domestico
-       departamento, 2, comercial
-       departamento, 3, oficina
-       casa,         4, oficina
+x = """Edificacion,Id,Tipo,Consumo
+       casa,         0, domestico,3.
+       departamento, 1, domestico,11.4
+       departamento, 2, comercial,0.1
+       departamento, 3, oficina,10.0
+       casa,         4, oficina,1.0
     """
 
 precios = {"oficina": 0.8, "comercial": 5.0, "domestico": 1.0}
@@ -11,19 +11,34 @@ precios = {"oficina": 0.8, "comercial": 5.0, "domestico": 1.0}
 def main(documento):
     lineas = documento.splitlines()
     total = 0
+    headers = [ e.strip() for e in lineas[0].split(",") ]
     for linea in lineas[1:]:
         if len(linea.strip()) > 0:
             fila = linea.split(",")
-            ed,id,tipo = [e.strip() for e in fila ]
-            print(f"ID:{id},{ed},{tipo}")
+            values = [ e.strip() for e in fila ]
+            ed, id_, tipo, consumo = values
+            consumo = float(consumo)
+            print(f"ID:{id_},{ed},{tipo},{consumo}")
+            escala = 1.0
             if ed == "casa":
-                total += 1.0 * precios[tipo]
+                if consumo > 1.0:
+                    escala = 5.0
+                else:
+                    escala = 1.1
+
+                total += 1.0 * escala * precios[tipo] * consumo
             elif ed == "departamento":
-                total += 0.5 * precios[tipo]
+                if consumo > 1.0:
+                    escala = 3.0
+                else:
+                    escala = 1.2
+
+                total += 0.5 * escala * precios[tipo] * consumo
             else:
                 print("desconocido")
     print(f"Consumo total: {total}")
 
 
 if __name__ == "__main__":
-    main(x)
+    for _ in range(10000):
+        main(x)
