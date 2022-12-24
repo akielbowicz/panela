a3d35ae4a74c4f6685790bce2a25209639befd51
Augusto Kielbowicz <augusto.kiel@gmail.com>
2022-12-23 17:14:52 -0800

    Arreglar tests

--- panela/servicios_y_edificios/servicios.py
+++ panela/servicios_y_edificios/servicios.py
@@ -14,12 +14,27 @@ class Agua(Servicio):
    def cobrar(self, edificacion):
        return edificacion.consumoDeAgua(self)
 
+   def costoParaDepartamento(self, parametros):
+        return super().costoParaDepartamento(parametros)/2
+
 class Gas(Servicio):
    
    def cobrar(self, edificacion):
-       return edificacion.consumoDeGas(self)
+        return edificacion.consumoDeGas(self)
 
+   def costoParaDepartamento(self, parametros):
+        return super().costoParaCasa(parametros) / 10
+    
+   def costoParaCasa(self, parametro):
+        return 100 * super().costoParaCasa(parametro)        
+    
 class Electricidad(Servicio):
    
    def cobrar(self, edificacion):
        return edificacion.consumoDeElectricidad(self)
+
+   def costoParaCasa(self, parametro):
+        return 10 * super().costoParaCasa(parametro) 
+        
+   def costoParaDepartamento(self, parametros):
+        return super().costoParaCasa(parametros) * 3.5
